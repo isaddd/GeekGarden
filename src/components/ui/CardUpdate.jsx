e42dd8b1c451a741d070eeaf746c31541f9f033e
Irsyad <irsyadbagas2@gmail.com>
2025-04-24 11:44:49 +0700

    first commit

--- src/components/ui/CardUpdate.jsx
+++ src/components/ui/CardUpdate.jsx
@@ -6,7 +6,7 @@ function CardUpdate(props) {
     <>
       <div key={key} className="rounded-lg relative md:mb-0 mb-20">
         <img src={img} alt={title} className="w-full" />
-        <div className="absolute -bottom-17 left-1/2 transform -translate-x-1/2 p-4 z-1 bg-[#F5F7FA] shadow-md rounded-md w-[84%]">
+        <div className="absolute -bottom-17 left-1/2 transform -translate-x-1/2 p-4 z-1 bg-[#F5F7FA] shadow-md rounded-md w-[86%]">
           <div className="md:text-2xl text-lg font-semibold mb-5">{title}</div>
           <a href="#" className="text-[#4CAF4F] font-semibold">
             Readmore →
